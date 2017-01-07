046942e6a07526f29a413e277d860cbe7fbd8376
Andrew Mc Nutt <mcnutt@uber.com>
2017-01-07 15:44:35 -0800

    update secret to env variables

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var TwitterPackage = require('twitter');
 
 var secret = {
-  consumer_key: 'xxx',
-  consumer_secret: 'xxx',
-  access_token_key: 'xxx',
-  access_token_secret: 'xxx'
+  consumer_key: TWITTER_CONSUMER_KEY,
+  consumer_secret: TWITTER_CONSUMER_SECRET,
+  access_token_key: TWITTER_ACCESS_KEY,
+  access_token_secret: TWITTER_ACCESS_SECRET
 }
 
 var Twitter = new TwitterPackage(secret);
-
-Twitter.post('statuses/update', {status: 'Hiya world!'},  function(error, tweet, response){
+Twitter.post('statuses/update', {status: 'Hiya world!'}, function(error, tweet, response){
   if(error){
     console.log(error);
   }
