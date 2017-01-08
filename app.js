12dbaef1db0af11d5c6ff77823100037899a4c0e
Andrew Mc Nutt <mcnutt@uber.com>
2017-01-07 17:01:33 -0800

    blind guess to see if this works

--- app.js
+++ app.js
@@ -21,3 +21,34 @@ Twitter.post('statuses/update', {
   console.log(tweet);  // Tweet body.
   console.log(response);  // Raw response object.
 });
+
+Twitter.stream('statuses/filter', {track: '@little_counter'}, function(stream) {
+
+  // ... when we get tweet data...
+  stream.on('data', function(tweet) {
+
+    // print out the text of the tweet that came in
+    console.log(tweet.text);
+
+    //build our reply object
+    var statusObj = {status: "Hi @" + tweet.user.screen_name + ", How are you?"}
+
+    //call the post function to tweet something
+    Twitter.post('statuses/update', statusObj,  function(error, tweetReply, response){
+
+      //if we get an error print it out
+      if(error){
+        console.log(error);
+      }
+
+      //print the text of the tweet we sent out
+      console.log(tweetReply.text);
+    });
+  });
+
+  // ... when we get an error...
+  stream.on('error', function(error) {
+    //print out the error
+    console.log(error);
+  });
+});
